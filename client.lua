eb0f96e8846bc02022a508b202d863b3508b3f8e
Valentin CHENE <valentin.chene.p@gmail.com>
2025-03-16 15:54:44 +0100

    Draw time

--- client.lua
+++ client.lua
@@ -1,19 +1,25 @@
+local voiceMode = nil
+local isDrawing = false
+local endTime = 0
+
 AddEventHandler("pma-voice:setTalkingMode", function(vm)
-    local voiceMode = Config.VoiceModes[vm]
+    voiceMode = Config.VoiceModes[vm]
     if (not voiceMode) then return end
 
+    endTime = GetGameTimer() + Config.DrawTime    
+    if (isDrawing) then return end
+    isDrawing = true
+
     Citizen.CreateThread(function()
-        local markerColor = voiceMode.color
-        local markerScale = voiceMode.distance
-        
         while true do
             Citizen.Wait(0)
-             
-            if (not IsControlPressed(0, Config.ControlKey)) then
-                break
+
+            if (GetGameTimer() > endTime) then
+                isDrawing = false
+                return
             end
 
-            DrawMarker(-1795314153, GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 0.0, Config.ZOffset), 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, markerScale, markerScale, Config.Height, markerColor.r, markerColor.g, markerColor.b, markerColor.a, 0, 0, 2, 0, 0, 0, 0)
+            DrawMarker(-1795314153, GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 0.0, Config.ZOffset), 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, voiceMode.distance, voiceMode.distance, Config.Height, voiceMode.color.r, voiceMode.color.g, voiceMode.color.b, voiceMode.color.a, 0, 0, 2, 0, 0, 0, 0)
         end
     end)
 end)
--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.ControlKey = 0x446258B6 -- Page UP
+Config.DrawTime = 1000 -- Time in milliseconds to draw the voice radius
 
 Config.ZOffset = -0.5 -- Z offset of the voice radius
 
@@ -9,15 +9,15 @@ Config.Height = 0.5 -- Height of the voice radius
 Config.VoiceModes = {
     [1] = { -- Whisper
         distance = 3.0,
-        color = {r = 0, g = 0, b = 255, a = 100}
+        color = {r = 255, g = 255, b = 0, a = 100}
     },
     [2] = { -- Normal
         distance = 8.0,
-        color = {r = 0, g = 255, b = 0, a = 100}
+        color = {r = 255, g = 255, b = 0, a = 100}
     },
     [3] = { -- Shouting
         distance = 15.0,
-        color = {r = 255, g = 0, b = 0, a = 100}
+        color = {r = 255, g = 255, b = 0, a = 100}
     },
     [4] = { -- Riding
         distance = 32.0,
