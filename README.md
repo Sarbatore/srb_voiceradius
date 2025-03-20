2c6b9505387660549f4e213ea2b3623e4e9631d3
Valentin CHENE <valentin.chene.p@gmail.com>
2025-03-20 15:40:48 +0100

    changes

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Optimized voice radius for proximity cycle of pma-voice.
 
 # Dependencies
 - pma-voice
+
+# Link
+- [Github](https://github.com/Sarbatore/srb_voiceradius)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,15 @@ game "rdr3"
 fx_version "adamant"
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 lua54 "yes"
+
 author "Sarbatore"
-version "1.0.0"
+description "Radius for voice proximity"
 
 client_scripts {
     "config.lua",
     "client.lua",
+}
+
+escrow_ignore {
+    "config.lua"
 }
